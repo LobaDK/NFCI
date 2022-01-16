0c0e4ef8d7cffb4b4cbfd1e8b914c1b8c7eab286
LobaDK <lobadk@lobadk.com>
2022-01-16 05:05:37 +0100

    Added Sharprompt package for interactable console.

--- EasyFFmpeg/Program.cs
+++ EasyFFmpeg/Program.cs
@@ -1,20 +1,23 @@
-﻿// Sets variables
-string UserChoice;
-string WebMUserChoice;
+﻿using Sharprompt; //loads the interactable console package
+// Sets variables
+//string UserChoice;
+//string WebMUserChoice;
 
 //main menu
 Start:
 Console.WriteLine("---------------Welcome---------------");
 Console.WriteLine("                  to                 ");
 Console.WriteLine("  Nicklo's FFmpeg Console Interface\n");
-Console.WriteLine("Please chose an option:");
-Console.WriteLine("Convert to (W)ebM");
-Console.WriteLine("Convert to (M)P4");
-Console.WriteLine("(E)xit the program");
-UserChoice = Console.ReadLine().ToUpper();
+var UserChoice = Prompt.Select("Please select an option", new[] { "WebM", "MP4", "Exit"});
+
+//console.writeline("please chose an option:");
+//console.writeline("convert to (w)ebm");
+//console.writeline("convert to (m)p4");
+//console.writeline("(e)xit the program");
+//userchoice = console.readline().toupper();
 
 //checks UserChoice
-if (UserChoice == "W") //UserChoice for converting to WebM
+if (UserChoice == "WebM") //UserChoice for converting to WebM
 {
     Console.Clear();
     Console.WriteLine("You've chosen to convert to WebM\n");
@@ -22,31 +25,36 @@ if (UserChoice == "W") //UserChoice for converting to WebM
     Console.WriteLine("(V)ariable");
     Console.WriteLine("(C)onstant");
     Console.WriteLine("(H)elp");
-    WebMUserChoice = Console.ReadLine();
+    WebMUserChoice = Console.ReadLine().ToUpper();
 
-    //checks WebmUserChoice
-    if (WebMUserChoice == "V") //WebmUserChoice for variable bitrate
+    if (WebMUserChoice == "H")
+    {
+        Console.Clear();
+        Console.WriteLine("\t\tVariable bitrate:");
+        Console.WriteLine("As it suggests, this means the bitrate is variable, and can increase or decrease per frame,");
+        Console.WriteLine("depending on how much dedicated data ffmpeg believes each frame needs");
+        Console.ReadKey();
+
+
+
+    }
+    else
     {
 
     }
-    else if (WebMUserChoice == "C") //WebMUserChoice for constant bitrate
-    {
-
-    }
-
 }
-else if (UserChoice == "M") //UserChoice for converting to MP4
+else if (UserChoice == "MP4") //UserChoice for converting to MP4
 {
 
 }
-else if (UserChoice == "E") //UserChoice for exiting the program
+else if (UserChoice == "Exit") //UserChoice for exiting the program
 {
     return;
 }
-else
-{
-    Console.WriteLine("{0} is not a valid option. Returning...",UserChoice);
-    Thread.Sleep(3000);
-    Console.Clear();
-    goto Start;
-}
+//else
+//{
+//    Console.WriteLine("{0} is not a valid option. Returning...",UserChoice);
+//    Thread.Sleep(3000);
+//    Console.Clear();
+//    goto Start;
+//}
