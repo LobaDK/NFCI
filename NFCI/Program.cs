e33a813b4c5b4e9d3aa1448c87dd2b217f117b70
LobaDK <lobadk@lobadk.com>
2022-01-17 10:54:43 +0100

    Added more code.

--- NFCI/Program.cs
+++ NFCI/Program.cs
@@ -1,45 +1,48 @@
 ﻿using Sharprompt; //loads the interactable console package
 // Sets variables
-//string UserChoice;
-//string WebMUserChoice;
+string WebMFileInput;
 
 //main menu
 Start:
 Console.WriteLine("---------------Welcome---------------");
 Console.WriteLine("                  to                 ");
 Console.WriteLine("  Nicklo's FFmpeg Console Interface\n");
-var UserChoice = Prompt.Select("Please select an option", new[] { "WebM", "MP4", "Exit"});
-
-//console.writeline("please chose an option:");
-//console.writeline("convert to (w)ebm");
-//console.writeline("convert to (m)p4");
-//console.writeline("(e)xit the program");
-//userchoice = console.readline().toupper();
+var UserChoice = Prompt.Select("Please select an option", new[] { "WebM", "MP4", "Info", "Exit"}); //Uses Sharprompt for the main manu's user input
 
 //checks UserChoice
 if (UserChoice == "WebM") //UserChoice for converting to WebM
 {
+    WebMStart:
     Console.Clear();
-    Console.WriteLine("You've chosen to convert to WebM\n");
-    Console.WriteLine("Please select whether you'd like to use Variable or Constant bitrate");
-    Console.WriteLine("(V)ariable");
-    Console.WriteLine("(C)onstant");
-    Console.WriteLine("(H)elp");
-    WebMUserChoice = Console.ReadLine().ToUpper();
+    var WebMUserChoice = Prompt.Select("You have selected WebM. Please select a bitrate option", new[] { "Variable", "Constant", "Help" }); //Uses Sharprompt for the WebM's bitrate options
 
-    if (WebMUserChoice == "H")
+    if (WebMUserChoice == "Help") //UserChoice for displaying a help section about bitrate options
     {
         Console.Clear();
-        Console.WriteLine("\t\tVariable bitrate:");
-        Console.WriteLine("As it suggests, this means the bitrate is variable, and can increase or decrease per frame,");
-        Console.WriteLine("depending on how much dedicated data ffmpeg believes each frame needs");
+        Console.WriteLine("\t\t\t\tVariable bitrate:");
+        Console.WriteLine("The bitrate is variable (can dynamically change) and can increase or decrease per frame,");
+        Console.WriteLine("depending on how much dedicated data ffmpeg believes each frame needs.");
+        Console.WriteLine("Variable bitrate is generally better and should always be used if possible,");
+        Console.WriteLine("especially if there's a mix of fast and slow moving action in the video.");
+        Console.WriteLine("However if a certain filesize is required, constant bitrate may be prefered.");
+        Console.WriteLine("For variable bitrate the option 'CRF' is used, which uses numbers between 0 and 63,");
+        Console.WriteLine("where higher means lower quality, and lower means higher quality. A value between 18 and 28");
+        Console.WriteLine("is often sufficient, though requires adjusting depending on the videos context.\n\n");
+        Console.WriteLine("\t\t\t\tConstant bitrate:");
+        Console.WriteLine("The bitrate is constant and each frame gets the same bitrate regardless of how much it requires.");
+        Console.WriteLine("This can end up wasting valuable data if there is barely any movement in the video,");
+        Console.WriteLine("while making faster parts of the video look poorly, if the bitrate isn't high enough.");
+        Console.WriteLine("This option is only prefered if a certain filesize is required\n");
+        Console.WriteLine("Press any key to continue");
         Console.ReadKey();
-
-
-
+        goto WebMStart;
     }
     else
     {
+        var WebMDisableAudio = Prompt.Confirm("Disable audio?", defaultValue: false);
+        Console.WriteLine("\n");
+        Console.WriteLine("Please select the video file. You can drag and drop the file.");
+        WebMFileInput = Console.ReadLine();
 
     }
 }
@@ -51,10 +54,15 @@ else if (UserChoice == "Exit") //UserChoice for exiting the program
 {
     return;
 }
-//else
-//{
-//    Console.WriteLine("{0} is not a valid option. Returning...",UserChoice);
-//    Thread.Sleep(3000);
-//    Console.Clear();
-//    goto Start;
-//}
+else if (UserChoice == "Info") //UserChoice for information about the program
+{
+    Console.Clear();
+    Console.WriteLine("This program was written with frustration and the will to learn.");
+    Console.WriteLine("Sharprompt was also used to create the interactable menu's");
+    Console.WriteLine("and all credit goes to it's respective developer(s)");
+    Console.WriteLine("at https://github.com/shibayan/Sharprompt \n");
+    Console.WriteLine("Press any key to continue");
+    Console.ReadKey();
+    Console.Clear();
+    goto Start;
+}
